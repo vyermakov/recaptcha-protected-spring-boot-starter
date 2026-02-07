ac4300f95f1e6883bcdbdc7c07f4bb239f3c2531
Slava Yermakov <v.yermakov@gmail.com>
2026-02-07 14:18:08 +0100

    Add @SuppressWarnings serial to RecaptchaValidationException

--- src/main/java/com/jeevision/security/recaptcha/RecaptchaValidationException.java
+++ src/main/java/com/jeevision/security/recaptcha/RecaptchaValidationException.java
@@ -3,6 +3,7 @@ package com.jeevision.security.recaptcha;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+@SuppressWarnings("serial")
 public class RecaptchaValidationException extends ResponseStatusException {
     public RecaptchaValidationException(String message) {
         super(HttpStatus.BAD_REQUEST, message);
